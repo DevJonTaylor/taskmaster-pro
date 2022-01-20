8ee2ae1883376ea84bd48790b847cff23e0da9f5
Jon Taylor <31380713+DevJonTaylor@users.noreply.github.com>
2022-01-19 23:31:56 -0600

    drag and drop features added

--- assets/js/script.js
+++ assets/js/script.js
@@ -173,5 +173,51 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+$('.card .list-group').sortable({
+  connectWith: $('.card .list-group'),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function() {
+    console.log("activate", this);
+  },
+  deactive: function() {
+    console.log("deactivate", this)
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArr = [];
+
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find('span')
+        .text()
+        .trim();
+
+      tempArr.push({
+        text,
+        date
+      })
+
+      var arrName = $(this)
+        .attr('id')
+        .replace('list-', '');
+
+      tasks[arrName] = tempArr;
+      saveTasks();
+    })
+  }
+})
+
 // load tasks for the first time
 loadTasks();
